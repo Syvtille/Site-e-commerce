c05e18af49e177e54e26b9cd9168eaff72db3587
Florian <escande.florian.chatco@gmail.com>
2023-03-17 09:35:15 +0100

    Refactor code in c-post and model and update v-post-list

--- src/controllers/c-post.php
+++ src/controllers/c-post.php
@@ -4,56 +4,11 @@ require('src/model.php');
 
 function post(){
     if(isset($_GET['identifiant']) && $_GET['identifiant']){
-        $port = 3306;
-        $server = "localhost";
-        $base = "s4-demo3";
-        $user = "s4-demo3";
-        $mdp = "30g3h@iNh?rhqTJx";
-
-        try {
-            $pdo = new PDO ("mysql:host=$server;port=$port;dbname=$base;charset=utf8", $user, $mdp);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (Exception $e) {
-            die('Erreur : ' . $e->getMessage());
-        }
-
-        function get_result($requete)
-        {
-            global $pdo;
-            $requete = $pdo->query($requete);
-            return $requete->fetch(PDO::FETCH_ASSOC);
-        }
-
-        function get_results($requete)
-        {
-            global $pdo;
-            $requete = $pdo->query($requete);
-            return $requete->fetchAll(PDO::FETCH_ASSOC);
-        }
-
-        function set_insert($table, $data)
-        {
-            global $pdo;
-
-            $requete = "";
-            $values = "";
-
-            foreach ($data as $key => $value) {
-                if ($key && $value) {
-                    if (!!$requete) {
-                        $requete .= ", ";
-                        $values .= "', '";
-                    }
-                    $requete .= $key;
-                    $values .= $value;
-                }
-            }
-            $requete = "INSERT INTO $table ($requete) VALUES ('$values')";
-            $stmt = $pdo->query($requete);
-        }
+//        $unPost =
     }
     else{
-
+        $lstPost = get_results("SELECT * FROM post WHERE statut=1");
+        postList($lstPost);
     }
 }
 
@@ -68,7 +23,7 @@ function postSimple()
     include('view/post/v-post.php');
 }
 
-function postList()
+function postList($lstPost)
 {
     $menu['page'] = "post";
 
