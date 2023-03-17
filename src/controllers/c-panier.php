fb516eb2261e10d05ecfea1c362d172aceacdd38
Florian <escande.florian.chatco@gmail.com>
2023-03-17 12:49:23 +0100

    Update c-panier.php to include page header and footer

--- src/controllers/c-panier.php
+++ src/controllers/c-panier.php
@@ -3,5 +3,12 @@
 require_once('src/model.php');
 
 function panier(){
+    $menu['page'] = "panier";
+
+    //Traitement
+    include('view/inc/inc.head.php');
+    include('view/inc/inc.header.php');
+    include('view/panier/v-panier.php');
+    include('view/inc/inc.footer.php');
 
 }
