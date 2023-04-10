64fa18a78db8fa4b6775dbb0380f74bbd570720c
Myrtille <escande.florian.chatco@gmail.com>
2023-04-10 16:10:49 +0200

    carousel

--- view/v-accueil.php
+++ view/v-accueil.php
@@ -1,8 +1,37 @@
 <?php
 global $nomSite;
 ?>
-<div class="banner">
-    <h2>Bienvenue sur <?= $nomSite; ?> !</h2>
+
+<!--<div class="banner">-->
+<!--    <h2>Bienvenue sur --><?php //= $nomSite; ?><!-- !</h2>-->
+<!--</div>-->
+
+<!--carousel-->
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner carousel-accueil">
+        <div class="carousel-item active">
+            <img src="../assets/img/banner-nouveaute.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/img/banner-fraise.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="../assets/img/banner-fructose.png" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
 
 <div class="container">
@@ -29,5 +58,5 @@ global $nomSite;
 </div>
 
 <footer>
-    &copy; <?= date("Y"); ?> <?=$nomSite; ?>. Tous droits réservés.
+    &copy; <?= date("Y"); ?> <?= $nomSite; ?>. Tous droits réservés.
 </footer>
