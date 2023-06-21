b8e6db0b813fb5c8f23455e1ea670dca76c967b7
Florian <escande.florian.chatco@gmail.com>
2023-06-21 15:09:14 +0200

    nav-results

--- client/src/controllers/c-recherche.php
+++ client/src/controllers/c-recherche.php
@@ -1,29 +1,12 @@
 <?php
-
-function recherche(){
-
+function recherche()
+{
     $menu['page'] = "recherche";
 
-    $urlApiGouvEntreprise = 'https://recherche-entreprises.api.gouv.fr/search?q=la%20poste&page=1&per_page=1';
+    $idPaiement = (isset($_GET['identifiant']) && $_GET['identifiant']) ? $_GET['identifiant'] : null;
 
     include('view/inc/inc.head.php');
     include('view/inc/inc.header.php');
     include('view/recherche/v-recherche.php');
     include('view/inc/inc.footer.php');
-}
-
-function rechercheEntreprise($query){
-
-}
-
-function rechercheAnnuaire($query){
-
-}
-
-function initCurl($url, $query){
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url . $query);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    return $curl;
 }
--- client/view/recherche/v-recherche.php
+++ client/view/recherche/v-recherche.php
@@ -1,40 +1,103 @@
 <h1>Recherche</h1>
 
 <div class="container">
-    <form action="" method="POST">
+    <form id="search-form">
         <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recherche" name="query" required>
+            <input type="text" class="form-control" placeholder="Recherche" id="searchQuery" required>
             <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="submit">Recherche</button>
+                <button class="btn btn-outline-secondary" type="button" id="searchButton">Recherche</button>
             </div>
         </div>
     </form>
 
     <div class="results" style="height:400px;overflow:auto;">
-        <table class="table table-striped">
+        <table class="table table-striped" id="resultsTable">
             <thead class='thead-dark'>
-            <tr><th>ID</th><th>Montant</th><th>Date</th><th>Référence</th><th>Action</th></tr>
+            <tr>
+                <th>Nom</th>
+                <th>Date de création</th>
+                <th>Catégorie</th>
+                <th>Détails</th>
+            </tr>
             </thead>
             <tbody>
-            <?php
-            if (isset($_POST['query'])) {
-                $query = $_POST['query'];
-                // À remplacer par la logique d'appel à votre API pour obtenir les résultats en fonction de la recherche.
-                // $results = call_your_api($query);
-                $results = [];  // mock
-
-                foreach ($results as $result) {
-                    echo "<tr>";
-                    echo "<td>" . $result['id_paiement'] . "</td>";
-                    echo "<td>" . number_format($result['montant'] / 100, 2, ',', ' ') . "€</td>";
-                    echo "<td>" . $result['date_paiement'] . "</td>";
-                    echo "<td>" . $result['id_unique'] . "</td>";
-                    echo '<td><a href="'.$urlSiteClient . 'paiement/' . $result['id_paiement'] . '/" class="btn btn-info">Voir Détails</a></td>';
-                    echo "</tr>";
-                }
-            }
-            ?>
+            <!-- Les résultats de recherche seront insérés ici par JavaScript -->
             </tbody>
         </table>
     </div>
+
+    <div id="pagination" class="mt-3">
+        <button class="btn btn-secondary" id="previousButton" disabled>Précédent</button>
+        <button class="btn btn-secondary" id="nextButton" disabled>Suivant</button>
+        <span id="pageInfo"></span>
+    </div>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+<script>
+    var currentPage = 1;
+    var totalPageCount = 1;
+
+    function updatePaginationButtons() {
+        var previousButton = document.getElementById('previousButton');
+        var nextButton = document.getElementById('nextButton');
+
+        previousButton.disabled = currentPage === 1;
+        nextButton.disabled = currentPage === totalPageCount;
+    }
+
+    function updatePageInfo() {
+        var pageInfo = document.getElementById('pageInfo');
+        pageInfo.textContent = 'Page ' + currentPage + ' / ' + totalPageCount;
+    }
+
+    function fetchResults() {
+        var searchQuery = document.getElementById('searchQuery').value;
+        var resultsTable = document.getElementById('resultsTable');
+
+        fetch('https://recherche-entreprises.api.gouv.fr/search?q=' + searchQuery + '&page=' + currentPage + '&per_page=10')
+            .then(response => response.json())
+            .then(data => {
+                // Supprimer les anciennes lignes de résultats
+                while (resultsTable.rows.length > 1) {
+                    resultsTable.deleteRow(1);
+                }
+
+                console.log(data.results);
+
+                // Ajouter les nouvelles lignes de résultats
+                data.results.forEach(enterprise => {
+                    var newRow = resultsTable.insertRow();
+                    newRow.insertCell(0).textContent = enterprise.nom_complet;
+                    var dateCreation = moment(enterprise.date_creation).format('DD/MM/YYYY');
+                    newRow.insertCell(1).textContent = dateCreation;
+                    newRow.insertCell(2).textContent = enterprise.categorie_entreprise;
+                    newRow.insertCell(3).innerHTML = '<a href="https://s4-gp98.kevinpecro.info/client/recherche/' + enterprise.siren + '" target="_blank">Voir Détails</a>';
+                });
+
+                totalPageCount = data.total_pages;
+                updatePaginationButtons();
+                updatePageInfo();
+            })
+            .catch(error => console.log(error));
+    }
+
+    document.getElementById('searchButton').addEventListener('click', function() {
+        currentPage = 1;
+        fetchResults();
+    });
+
+    document.getElementById('previousButton').addEventListener('click', function() {
+        if (currentPage > 1) {
+            currentPage--;
+            fetchResults();
+        }
+    });
+
+    document.getElementById('nextButton').addEventListener('click', function() {
+        if (currentPage < totalPageCount) {
+            currentPage++;
+            fetchResults();
+        }
+    });
+</script>
